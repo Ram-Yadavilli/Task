bf4b2e49233b3afc1edc23f4a407ef75575263f6
Ram-Yadavilli <ram.yadavilli21@gmail.com>
2023-10-19 14:39:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import "./App.css";
+
+import List from "./List";
+
+import { AiOutlineUnorderedList } from "react-icons/ai";
+import { BsGrid } from "react-icons/bs";
+
+import { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("Blue");
+  const [a, setA] = useState(true);
+  const [b, setB] = useState(false);
+
+  useEffect(() => {
+    let x = async function () {
+      let response = await fetch(
+        "https://mocki.io/v1/0934df88-6bf7-41fd-9e59-4fb7b8758093"
+      );
+      let d = await response.json();
+      setData(d.data);
+      console.log(d.data);
+    };
+    x();
+  }, []);
+
+  let change = (event) => {
+    setSearch(event.target.value);
+  };
+
+  let b1 = () => {
+    setA(true);
+    setB(false);
+  };
+
+  let b2 = () => {
+    setA(false);
+    setB(true);
+  };
+
+  let z = a ? "l" : "g";
+
+  return (
+    <div className="main">
+      <h1>PLP</h1>
+      <div className="in">
+        <input
+          type="text"
+          onChange={change}
+          placeholder="place something to search..."
+        />
+        <button className="ico" onClick={b1}>
+          <AiOutlineUnorderedList style={{ height: "20px", width: "20px" }} />
+        </button>
+        <button className="ico" onClick={b2}>
+          <BsGrid style={{ height: "15px", width: "15px" }} />
+        </button>
+      </div>
+
+      <ul className={z}>
+        {data.map((i) => (
+          <List detail={i} search={search} a={a} b={b} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/List.js
@@ -0,0 +1,47 @@
+import "./List.css";
+
+const List = (props) => {
+  const { detail, search, a } = props;
+  const { product_badge, product_image, product_title, product_variants } =
+    detail;
+  let y = a ? "list" : "grid";
+  let bt = a ? "btn" : "btn1";
+  let im = a ? "im1" : "im2";
+  let h = a ? "h" : "h1";
+
+  return (
+    <li className={y}>
+      <div className="ic">
+        <button className={bt}>{product_badge}</button>
+        <img src={product_image} alt="cap_img" className={im} />
+      </div>
+
+      <div className="ic">
+        <h4 className={h}>{product_title}</h4>
+
+        {product_variants[0].v1.includes(search) ||
+        product_variants[0].v1.includes(search) ? (
+          <p className="co">{product_variants[0].v1}</p>
+        ) : (
+          <p>{product_variants[0].v1}</p>
+        )}
+
+        {product_variants[1].v2.includes(search) ||
+        product_variants[1].v2.includes(search) ? (
+          <p className="co">{product_variants[1].v2}</p>
+        ) : (
+          <p>{product_variants[1].v2}</p>
+        )}
+
+        {product_variants[2].v3.includes(search) ||
+        product_variants[2].v3.includes(search) ? (
+          <p className="co">{product_variants[2].v3}</p>
+        ) : (
+          <p>{product_variants[2].v3}</p>
+        )}
+      </div>
+    </li>
+  );
+};
+
+export default List;
